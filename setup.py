c8cddee6b1feb25af193cd162d51209907cef58d
the16thpythonist <jonseb1998@gmail.com>
2018-12-28 14:13:02 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+# the imports
+from setuptools import setup
+
+
+setup(
+    name='statepub',
+    version='0.0.0.0',
+    description='A program for publishing the current state of a computing machine to the MQTT network',
+    url='https://github.com/the16thpythonist/statepub',
+    author='Jonas Teufel',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=[
+        'statepub'
+    ],
+    install_requires=[
+
+    ],
+    python_requires='~=3.5',
+    zip_safe=False
+)
