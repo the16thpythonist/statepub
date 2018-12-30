d7eb303396a783fcb2b79071ee363b5ff4dcaab0
the16thpythonist <jonseb1998@gmail.com>
2018-12-30 14:11:25 +0100

    commented the statepublisher object

--- statepub/publish.py
+++ statepub/publish.py
@@ -8,12 +8,37 @@ from statepub.state import StateInterface
 
 
 class StatePublisher:
+    """
+    This object is used to publish the information contained within state objects to the mqtt network.
 
+    A word on the topics:
+    The final topic to publish to will consist of three "path pieces". The base path being dictated by the initial
+    parameters given during the creation of the object, the further path being given as parameter to this function
+    and the top most topic being the key string of the given dictionary
+
+    {object wide base path}/{method wide base path}/{dictionary key}
+
+    CHANGELOG
+
+    Added 29.12.2018
+    """
     # The mqtt client object expects an ID. This static field will keep track of how many Publisher objects have been
     # created and supplies each one with a new ID.
     id_counter = 0
 
     def __init__(self, topic_base, broker_ip, broker_port=1883):
+        """
+        The Constructor.
+
+        CHANGELOG
+
+        Added 29.12.2018
+
+        :param str topic_base:  The string which is to be used as the "base path" of the topic to which the info
+                                should be published.
+        :param str broker_ip:   The string of the IP of the machine on which the mqtt broker is running
+        :param int broker_port: The port on which the mqtt is running. DEFAULT is 1883 for mosquitto mqtt broker
+        """
         self.topic_base = topic_base
 
         # We get the ID of the current object by using the current state of the static id counter which always provides
@@ -32,7 +57,19 @@ class StatePublisher:
         self.client.loop_start()
 
     def publish(self, *states, topic=''):
+        """
+        Given multiple State objects this method will publish all the information/values contained within the attributes
+        of those objects.
+        A base topic path of the publishing can be given.
 
+        CHANGELOG
+
+        Added 29.12.2018
+
+        :param StateInterface states:   A List of all the states to be published to the network
+        :param str topic:               A base path for the topics to be published to
+        :return:
+        """
         # What we do here is we get the dictionary representations of all states and then merge them together into one
         # big dictionary and then call the method which publishes the dictionary.
         combined_dict = {
@@ -47,7 +84,25 @@ class StatePublisher:
         self.publish_dict(combined_dict, topic)
 
     def publish_dict(self, dictionary, topic):
+        """
+        Takes a dictionary and publishes all values as values to the MQTT network, with the keys of the dict being the
+        topics to publish to.
 
+        A word on the topics:
+        The final topic to publish to will consist of three "path pieces". The base path being dictated by the initial
+        parameters given during the creation of the object, the further path being given as parameter to this function
+        and the top most topic being the key string of the given dictionary
+
+        {object wide base path}/{method wide base path}/{dictionary key}
+
+        CHANGELOG
+
+        Added 29.12.2018
+
+        :param dictionary:
+        :param topic:
+        :return:
+        """
         for key, value in dictionary.items():
             # Creating the topic to publish the value to by joining the actual name of the topic, which is the key
             # string in this dict with the base path given to the publisher object and the base path given to this
@@ -57,5 +112,14 @@ class StatePublisher:
             self.client.publish(topic_string, str(value))
 
     def close(self):
+        """
+        This method properly closes down the MQTT client object, which is used for network communication.
+
+        CHANGELOG
+
+        Added 29.12.2018
+
+        :return:
+        """
         self.client.disconnect()
         self.client.loop_stop()
